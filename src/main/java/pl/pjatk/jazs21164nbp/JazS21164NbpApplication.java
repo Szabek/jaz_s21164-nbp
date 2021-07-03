fc8cd68f6750d33d8d3a2eefc9419b6b907baaa6
Szabek <mrschabpl@gmail.com>
2021-07-03 14:01:42 +0200

    model for NBP api created

---
+++ src/main/java/pl/pjatk/jazs21164nbp/JazS21164NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21164nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21164NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS21164NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jazs21164nbp/JazS21164NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21164nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21164NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
